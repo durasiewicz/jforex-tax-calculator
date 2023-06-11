ab45b9d1b0a41082cdeb43f9f38f57835a03e052
Artur Durasiewicz <artur@durasiewi.cz>
2023-06-11 15:48:08 +0200

    Get data from NBP API.

--- JForexTaxCalculator/Program.fs
+++ JForexTaxCalculator/Program.fs
@@ -1,4 +1,6 @@
 ﻿open System
+open System.Net.Http
+open System.Threading.Tasks
 
 [<Literal>]
 let nbpApiDayRangeLimit = 367
@@ -10,19 +12,46 @@ type NbpExchangeRate =
     { No: string
       EffectiveDate: DateTime
       Mid: decimal }
-    
-let rec getDateRanges (startDate : DateTime) (endDate : DateTime) (dayShift : int) =
+
+let rec getDateRanges (startDate: DateTime) (endDate: DateTime) (dayShift: int) =
     seq {
         if startDate.AddDays(dayShift) >= endDate then
             yield (startDate, endDate)
-        elif dayShift % nbpApiDayRangeLimit = 0 then 
+        elif dayShift % nbpApiDayRangeLimit = 0 then
             yield (startDate, startDate.AddDays(dayShift))
-            yield! getDateRanges (startDate.AddDays(float(dayShift + 1))) endDate 1
+            yield! getDateRanges (startDate.AddDays(float (dayShift + 1))) endDate 1
         else
             yield! getDateRanges startDate endDate (dayShift + 1)
     }
 
-let ranges = getDateRanges (new DateTime(2022, 1, 1)) (new DateTime(2023,5,1)) 1
-let x = ranges |> Seq.toList
+let getNbpRates (currencySymbol: string) (dateRanges: List<DateTime * DateTime>) =
+    async {
+        let httpClient = new HttpClient()
+
+        let rec getRate accumulator (dateRanges: List<DateTime * DateTime>) =
+            match dateRanges with
+            | [] -> accumulator
+            | (startDate, endDate) :: t ->
+                let uri =
+                    new Uri(
+                        $"""http://api.nbp.pl/api/exchangerates/rates/A/{currencySymbol}/{startDate.ToString("yyyy-MM-dd")}/{endDate.ToString("yyyy-MM-dd")}/?format=json"""
+                    )
+
+                getRate (accumulator @ [ httpClient.GetStringAsync(uri) ]) t
+
+        let getRateTasks = getRate [] dateRanges
+
+        Task.WhenAll(getRateTasks) |> Async.AwaitTask |> ignore
+
+        return getRateTasks |> List.map (fun x -> x.Result)
+    }
+
+let ranges =
+    getDateRanges (new DateTime(2022, 1, 1)) (new DateTime(2023, 5, 1)) 1
+    |> Seq.toList
+    |> getNbpRates "EUR"
+    |> Async.RunSynchronously
+
+let x = ranges
 
 printfn "Hello from F#"
